a5ca8cc467b7492ebaca072012724b7e9cbe72ff
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-27 12:14:52 -0400

    Adding the test to this repo

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,66 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void firstTest() throws Exception {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://teamorg2-dev-ed.lightning.force.com/lightning/o/Contact/list?filterName=Recent");
+
+        WebElement username = driver.findElement(By.xpath("//input[@id='username']"));
+        WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='Login']"));
+
+        username.sendKeys("incubatorteam@oktana.com");
+        password.sendKeys("incubator2022");
+        loginButton.click();
+
+        Thread.sleep(8000);
+
+//        WebElement selectAllContactsCheckbox = driver.findElement(By.xpath("(//span[@class='slds-checkbox--faux'])[1]"));
+        WebElement selectRogerContact = driver.findElement(By.xpath("(//a[text()='Roger Ruiz']/ancestor::tr//td/following-sibling::td[@class='slds-cell-edit cellContainer'])[1]"));
+        selectRogerContact.click();
+
+        WebElement sendListEmailButton = driver.findElement(By.xpath("//div[contains(text(), 'Send List Email')]"));
+        sendListEmailButton.click();
+
+        Thread.sleep(4000);
+
+        WebElement subjectField = driver.findElement(By.xpath("//input[@placeholder='Enter Subject...']"));
+        subjectField.sendKeys("Incubator Team - Github Actions funciona !!! :)");
+
+        Thread.sleep(3000);
+
+        WebElement emailBodyIframe = driver.findElement(By.xpath("//div[@class='content iframe-parent']//iframe"));
+        driver.switchTo().frame(emailBodyIframe);
+
+        Thread.sleep(2000);
+
+        WebElement emailBody = driver.findElement(By.xpath("//div[@id='cke_editor']//iframe"));
+        driver.switchTo().frame(emailBody);
+
+        Thread.sleep(1000);
+
+        WebElement emailBodyTextArea = driver.findElement(By.cssSelector("body"));
+        emailBodyTextArea.sendKeys("Este email fue enviado automaticamente usando workflows en " +
+                                           "Github Actions con Selenium test. Se ejecuto y se ejecutara al hacerle push a la rama principal. " +
+                                           "Github Repo: https://github.com/rogeruiz22/martes171119.git");
+
+        Thread.sleep(2000);
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().parentFrame();
+
+        WebElement sendButton = driver.findElement(By.xpath("//button[text()='Send']"));
+        sendButton.click();
+
+        Thread.sleep(2000);
+
+        driver.quit();
     }
 }
