4c7418836f9992776230b8a431dec98af46730cc
Roger Luis Ruiz Mahr <104862925+rogeruiz22@users.noreply.github.com>
2022-06-01 17:21:03 -0400

    Update AnotherTest.java

--- src/test/java/org/example/AnotherTest.java
+++ src/test/java/org/example/AnotherTest.java
@@ -31,7 +31,7 @@ public class AnotherTest {
 
         Thread.sleep(3000);
 
-        WebElement composeButton = driver.findElement(By.xpath("//div[text()='Compose']"));
+        WebElement composeButton = driver.findElement(By.xpath("//div[@class='T-I T-I-KE L3']"));
         composeButton.click();
 
         Thread.sleep(2000);
