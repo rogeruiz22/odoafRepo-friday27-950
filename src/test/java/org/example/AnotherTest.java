5a2779be577564f2d93b2531b2bb0b8a57684ff2
Roger Luis Ruiz Mahr <104862925+rogeruiz22@users.noreply.github.com>
2022-06-02 11:36:10 -0400

    Update AnotherTest.java

--- src/test/java/org/example/AnotherTest.java
+++ src/test/java/org/example/AnotherTest.java
@@ -34,9 +34,9 @@ public class AnotherTest {
         WebElement nextButton2 = driver.findElement(By.xpath("//span[text()='Siguiente' or text()='Next']"));
         nextButton2.click();
 
-        Thread.sleep(8000);
+        Thread.sleep(10000);
 
-        WebElement composeButton = driver.findElement(By.xpath("//div[text()='Redactar' or text()='Compose']"));
+        WebElement composeButton = driver.findElement(By.xpath("//div[contains(text(), 'Compose')] | //div[contains(text(), 'Redactar')]"));
         composeButton.click();
 
         Thread.sleep(2000);
